ece018d0a557096d44d89b8fb60009917d0f20e9
Roku1234567 <s1f102401595@iniad.org>
2024-05-03 21:07:25 +0900

    全方向

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -117,6 +117,102 @@ const Home = () => {
         }
       }
     }
+    if (board[y - 1][x - 1] !== undefined && board[y - 1][x - 1] === 3 - turnColor) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(3 - turnColor);
+      setBoard(newBoard);
+      for (let a = 0; a < 8; a++) {
+        if (board[y - 2][x - 2] === 0) {
+          break;
+        }
+        console.table(newBoard);
+        if (turnColor === 1) {
+          newBoard[y][x] = turnColor;
+          newBoard[y - a][x - a] = turnColor;
+          if (board[y - a][x - a] !== undefined && board[y - a][x - a] === turnColor) {
+            break;
+          }
+        } else if (turnColor === 2) {
+          newBoard[y][x] = turnColor;
+          newBoard[y - a][x - a] = turnColor;
+          if (board[y - a][x - a] !== undefined && board[y - a][x - a] === turnColor) {
+            break;
+          }
+        }
+      }
+    }
+    if (board[y + 1][x - 1] !== undefined && board[y + 1][x - 1] === 3 - turnColor) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(3 - turnColor);
+      setBoard(newBoard);
+      for (let a = 0; a < 8; a++) {
+        if (board[y + 2][x - 2] === 0) {
+          break;
+        }
+        console.table(newBoard);
+        if (turnColor === 1) {
+          newBoard[y][x] = turnColor;
+          newBoard[y + a][x - a] = turnColor;
+          if (board[y + a][x - a] !== undefined && board[y + a][x - a] === turnColor) {
+            break;
+          }
+        } else if (turnColor === 2) {
+          newBoard[y][x] = turnColor;
+          newBoard[y + a][x - a] = turnColor;
+          if (board[y + a][x - a] !== undefined && board[y + a][x - a] === turnColor) {
+            break;
+          }
+        }
+      }
+    }
+    if (board[y - 1][x + 1] !== undefined && board[y - 1][x + 1] === 3 - turnColor) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(3 - turnColor);
+      setBoard(newBoard);
+      for (let a = 0; a < 8; a++) {
+        if (board[y - 2][x + 2] === 0) {
+          break;
+        }
+        console.table(newBoard);
+        if (turnColor === 1) {
+          newBoard[y][x] = turnColor;
+          newBoard[y - a][x + a] = turnColor;
+          if (board[y - a][x + a] !== undefined && board[y - a][x + a] === turnColor) {
+            break;
+          }
+        } else if (turnColor === 2) {
+          newBoard[y][x] = turnColor;
+          newBoard[y - a][x + a] = turnColor;
+          if (board[y - a][x + a] !== undefined && board[y - a][x + a] === turnColor) {
+            break;
+          }
+        }
+      }
+    }
+    if (board[y + 1][x + 1] !== undefined && board[y + 1][x + 1] === 3 - turnColor) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(3 - turnColor);
+      setBoard(newBoard);
+      for (let a = 0; a < 8; a++) {
+        if (board[y + 2][x + 2] === 0) {
+          break;
+        }
+        console.table(newBoard);
+        if (turnColor === 1) {
+          newBoard[y][x] = turnColor;
+          newBoard[y + a][x + a] = turnColor;
+          if (board[y + a][x + a] !== undefined && board[y + a][x + a] === turnColor) {
+            break;
+          }
+        } else if (turnColor === 2) {
+          newBoard[y][x] = turnColor;
+          newBoard[y + a][x + a] = turnColor;
+          if (board[y + a][x + a] !== undefined && board[y + a][x + a] === turnColor) {
+            break;
+          }
+        }
+      }
+    }
   };
   return (
     <div className={styles.container}>
